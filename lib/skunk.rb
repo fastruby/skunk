089b4ac8a7c0b365190f40287115c0474cf2465e
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2019-10-15 16:23:27 -0400

    Add documentation

--- lib/skunk.rb
+++ lib/skunk.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 require "skunk/version"
 
+# Knows how to calculate the `StinkScore` for each file analyzed by `RubyCritic`
+# and `SimpleCov`
 module Skunk
-  # Your code goes here...
 end
