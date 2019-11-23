5332b864e7a49fce5b011756b1560aa058b958b9
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2019-11-22 23:08:56 -0500

    Add support for --help and improve message for usage indications

--- lib/skunk/cli/application.rb
+++ lib/skunk/cli/application.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
+require "rubycritic/cli/options"
+require "rubycritic/cli/application"
+
 require "skunk"
 require "skunk/rubycritic/analysed_module"
 require "skunk/cli/options"
 require "skunk/cli/command_factory"
 
-require "rubycritic/cli/application"
-
 module Skunk
   module Cli
     # Knows how to execute command line commands
     class Application < RubyCritic::Cli::Application
+      def initialize(argv)
+        @options = Skunk::Cli::Options.new(argv)
+      end
+
       def execute
         parsed_options = @options.parse.to_h
         reporter = Skunk::Cli::CommandFactory.create(parsed_options).execute
--- lib/skunk/cli/options.rb
+++ lib/skunk/cli/options.rb
@@ -1,10 +1,34 @@
 # frozen_string_literal: true
 
-require "rubycritic/cli/options"
+require "skunk/cli/options/argv"
+require "rubycritic/cli/options/file"
 
 module Skunk
   module Cli
-    class Options < RubyCritic::Cli::Options
+    # Knows how to parse options passed to the CLI application
+    class Options
+      attr_reader :argv_options, :file_options
+
+      def initialize(argv)
+        @argv_options = Argv.new(argv)
+        @file_options = RubyCritic::Cli::Options::File.new
+      end
+
+      def parse
+        argv_options.parse
+        file_options.parse
+        self
+      end
+
+      # :reek:NilCheck
+      def to_h
+        file_hash = file_options.to_h
+        argv_hash = argv_options.to_h
+
+        file_hash.merge(argv_hash) do |_, file_option, argv_option|
+          Array(argv_option).empty? ? file_option : argv_option
+        end
+      end
     end
   end
 end
