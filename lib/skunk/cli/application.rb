dd787926ecbe62d0c3b65998bb7ef75d9f4f1e26
Manuel Barros Reyes <manuca@gmail.com>
2020-09-18 14:46:19 -0300

    warn when there's no test coverage

--- lib/skunk/cli/application.rb
+++ lib/skunk/cli/application.rb
@@ -12,11 +12,15 @@ module Skunk
   module Cli
     # Knows how to execute command line commands
     class Application < RubyCritic::Cli::Application
+      COVERAGE_FILE = "coverage/.resultset.json"
+
       def initialize(argv)
         @options = Skunk::Cli::Options.new(argv)
       end
 
       def execute
+        warn_coverage_info unless File.exist?(COVERAGE_FILE)
+
         parsed_options = @options.parse.to_h
         reporter = Skunk::Cli::CommandFactory.create(parsed_options).execute
         print(reporter.status_message)
@@ -24,6 +28,13 @@ module Skunk
       rescue OptionParser::InvalidOption => error
         warn "Error: #{error}"
         STATUS_ERROR
+      end
+
+      private
+
+      def warn_coverage_info
+        warn "warning: Couldn't find coverage info at #{COVERAGE_FILE}."
+        warn "warning: Having no coverage metrics will make your SkunkScore worse."
       end
     end
   end
