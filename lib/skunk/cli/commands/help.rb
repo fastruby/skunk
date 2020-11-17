cb804fc156ec2a8bd16a4614b24492ef1908799b
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2020-11-16 23:23:35 -0500

    Refactor code to use a more organized sharing mechanism
    I wanted to output the shared_url right after everything,
    so I had to rearrange the code in this PR.

--- lib/skunk/cli/commands/help.rb
+++ lib/skunk/cli/commands/help.rb
@@ -6,7 +6,17 @@ require "rubycritic/commands/help"
 module Skunk
   module Cli
     module Command
-      class Help < RubyCritic::Command::Help
+      # Knows how to guide user into using `skunk` properly
+      class Help < Skunk::Cli::Command::Base
+        # Outputs a help message
+        def execute
+          puts options[:help_text]
+          status_reporter
+        end
+
+        private
+
+        attr_reader :options, :status_reporter
       end
     end
   end
---
+++ lib/skunk/cli/commands/status_sharer.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+require "net/http"
+require "net/https"
+require "json"
+
+require "skunk/cli/commands/status_reporter"
+
+module Skunk
+  module Command
+    # Knows how to share status to an API
+    class StatusSharer < Skunk::Command::StatusReporter
+      attr_reader :status_message
+
+      DEFAULT_URL = "https://skunk.fastruby.io"
+      def status_reporter=(status_reporter)
+        self.analysed_modules = status_reporter.analysed_modules
+        self.score = analysed_modules.score
+      end
+
+      def share
+        return "" if not_sharing?
+
+        response = post_payload
+
+        @status_message =
+          if Net::HTTPOK === response
+            data = JSON.parse response.body
+            "Shared at: #{File.join(base_url, data['id'])}"
+          else
+            "Error sharing report: #{response}"
+          end
+      end
+
+      private
+
+      # :reek:UtilityFunction
+      def base_url
+        ENV["SHARE_URL"] || DEFAULT_URL
+      end
+
+      def json_summary
+        result = {
+          total_skunk_score: total_skunk_score,
+          analysed_modules_count: analysed_modules_count,
+          skunk_score_average: skunk_score_average,
+          skunk_version: Skunk::VERSION
+        }
+
+        if worst
+          result[:worst_skunk_score] = {
+            file: worst.pathname.to_s,
+            skunk_score: worst.skunk_score
+          }
+        end
+
+        result
+      end
+
+      def json_results
+        sorted_modules.map(&:to_hash)
+      end
+
+      # :reek:UtilityFunction
+      def not_sharing?
+        ENV["SHARE"] != "true" && ENV["SHARE_URL"].to_s == ""
+      end
+
+      def payload
+        {
+          "entries" => json_results,
+          "summary" => json_summary,
+          "options" => {
+            "compare" => "false"
+          }
+        }
+      end
+
+      # :reek:TooManyStatements
+      def post_payload
+        req = Net::HTTP::Post.new(url)
+
+        http = Net::HTTP.new(url.hostname, url.port)
+        if url.scheme == "https"
+          http.use_ssl = true
+          http.ssl_version = :TLSv1_2
+        end
+
+        http.start do |connection|
+          connection.request req
+        end
+      end
+
+      def url
+        URI(File.join(base_url, "reports"))
+      end
+    end
+  end
+end
