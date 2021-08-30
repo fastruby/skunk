8c9410508c3154ab483823a0ccc516492512ea30
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2021-08-30 19:31:11 -0400

    Add test coverage for version command

---
+++ test/lib/skunk/cli/commands/version_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+require "skunk/cli/commands/version"
+require "skunk/cli/options"
+
+describe Skunk::Command::Version do
+  describe "#execute" do
+    MSG = Skunk::VERSION
+
+    it "outputs the right version message" do
+      options = ["--version"]
+      opts = Skunk::Cli::Options.new(options).parse
+      subject = Skunk::Command::Version.new(opts.to_h)
+
+      assert_output(MSG) do
+        subject.execute
+      end
+    end
+  end
+end
