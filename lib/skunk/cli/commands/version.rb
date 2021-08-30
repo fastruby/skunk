5583989bd0795d03a1363638f744e35a83e72de7
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2021-08-30 19:36:39 -0400

    Add Cli module to be consistent across commands

--- lib/skunk/cli/commands/version.rb
+++ lib/skunk/cli/commands/version.rb
@@ -4,12 +4,14 @@ require "rubycritic/commands/version"
 
 # nodoc #
 module Skunk
-  module Command
-    # Shows skunk version
-    class Version < RubyCritic::Command::Version
-      def execute
-        print Skunk::VERSION
-        status_reporter
+  module Cli
+    module Command
+      # Shows skunk version
+      class Version < RubyCritic::Command::Version
+        def execute
+          print Skunk::VERSION
+          status_reporter
+        end
       end
     end
   end
