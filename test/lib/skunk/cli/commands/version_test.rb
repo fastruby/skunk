a5d86d7639f4db47e54ed2f64ae7ca8c224de0c1
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2021-08-30 19:36:59 -0400

    Use updated reference to test Version command

--- test/lib/skunk/cli/commands/version_test.rb
+++ test/lib/skunk/cli/commands/version_test.rb
@@ -5,14 +5,14 @@ require "test_helper"
 require "skunk/cli/commands/version"
 require "skunk/cli/options"
 
-describe Skunk::Command::Version do
+describe Skunk::Cli::Command::Version do
   describe "#execute" do
     MSG = Skunk::VERSION
 
     it "outputs the right version message" do
       options = ["--version"]
       opts = Skunk::Cli::Options.new(options).parse
-      subject = Skunk::Command::Version.new(opts.to_h)
+      subject = Skunk::Cli::Command::Version.new(opts.to_h)
 
       assert_output(MSG) do
         subject.execute
