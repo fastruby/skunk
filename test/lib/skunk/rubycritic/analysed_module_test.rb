be0136d1732fc97fe1b1b766de4012b63956986e
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2021-08-30 21:27:02 -0400

    Updated churn expectation

--- test/lib/skunk/rubycritic/analysed_module_test.rb
+++ test/lib/skunk/rubycritic/analysed_module_test.rb
@@ -61,7 +61,7 @@ describe RubyCritic::AnalysedModule do
         file: "samples/rubycritic/analysed_module.rb",
         skunk_score: 58.88,
         churn_times_cost: 2.94,
-        churn: 4,
+        churn: 5,
         cost: 0.59,
         coverage: 0.0
       }
