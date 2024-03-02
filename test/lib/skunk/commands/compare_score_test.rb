c7d1686cff32e141ed832ae8fc1ed7249702ba87
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2024-03-02 12:07:50 -0500

    Address deprecation warning (minitest)

--- test/lib/skunk/commands/compare_score_test.rb
+++ test/lib/skunk/commands/compare_score_test.rb
@@ -18,24 +18,22 @@ describe Skunk::Command::CompareScore do
   describe "#score_evolution_message" do
     it "computes skunk score_evolution_message with negative impact" do
       compare_score = Skunk::Command::CompareScore.new("main", "feature", 10.1, 12.1)
-      compare_score.score_evolution_message.must_equal "Skunk score average is 20% worse \n"
+      _(compare_score.score_evolution_message).must_equal "Skunk score average is 20% worse \n"
     end
 
     it "computes skunk score_evolution_message with positive impact" do
       compare_score = Skunk::Command::CompareScore.new("main", "feature", 12.1, 8)
-      compare_score.score_evolution_message.must_equal "Skunk score average is 34% better \n"
+      _(compare_score.score_evolution_message).must_equal "Skunk score average is 34% better \n"
     end
 
     it "computes skunk score_evolution_message when base_branch_score is 0" do
       compare_score = Skunk::Command::CompareScore.new("main", "feature", 0, 2)
-      compare_score
-        .score_evolution_message
-        .must_equal "Skunk score average is Infinitely worse \n"
+      _(compare_score.score_evolution_message).must_equal "Skunk score average is Infinitely worse \n"
     end
 
     it "computes skunk score_evolution_message when feature_branch_score is 0" do
       compare_score = Skunk::Command::CompareScore.new("main", "feature", 10, 0)
-      compare_score.score_evolution_message.must_equal "Skunk score average is 100% better \n"
+      _(compare_score.score_evolution_message).must_equal "Skunk score average is 100% better \n"
     end
   end
 end
